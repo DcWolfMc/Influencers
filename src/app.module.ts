34c2c2e44bbf90d9109731782e1c6582fa6e262d
Daniel <danielmcolares@gmail.com>
2024-09-01 15:18:01 -0300

    feat: logger module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
-import { ThrottlerGuard } from '@nestjs/throttler';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { envSchema } from './env';
 import { InfluencersModule } from './influencers/influencers.module';
 import { UsersModule } from './users/users.module';
 import { BrandsModule } from './brands/brands.module';
+import { LoggerModule } from './logger/logger.module';
 @Module({
   imports: [
     DatabaseModule,
@@ -21,6 +22,11 @@ import { BrandsModule } from './brands/brands.module';
     InfluencersModule,
     UsersModule,
     BrandsModule,
+    ThrottlerModule.forRoot([
+      { name: 'short', ttl: 1000, limit: 3 },
+      { name: 'long', ttl: 60000, limit: 100 },
+    ]),
+    LoggerModule,
   ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
