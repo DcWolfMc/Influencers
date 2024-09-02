790aa47ee15dee6e4d731413e486ece6fa2cadac
Daniel <danielmcolares@gmail.com>
2024-09-02 17:22:46 -0300

    styles: fix some colors

--- frontend/src/page/login/login.tsx
+++ frontend/src/page/login/login.tsx
@@ -5,14 +5,14 @@ export const Login = () => {
     
   }
   return (
-    <div className="p-8 flex flex-col bg-slate-500 rounded-md gap-8 shadow-md">
+    <div className="p-8 flex flex-col bg-slate-600 rounded-md gap-8 shadow-md">
       <strong className="text-xl">Entrar</strong>
       <form action="" onSubmit={handleSubmit} className="flex flex-col gap-8">
         <label htmlFor="email" className="flex flex-col gap-2">
           <span className="font-bold">Email: </span>
           <input
             id="email"
-            className="p-2 rounded-md w-[416px] bg-slate-700 text-slate-100 border-2 border-transparent"
+            className="p-2 rounded-md w-[416px] bg-slate-800 text-slate-100 border-2 border-transparent"
             type="email"
             placeholder="Digite seu E-mail"
           />
@@ -21,7 +21,7 @@ export const Login = () => {
           <span className="font-bold">Senha: </span>
           <input
             id="password"
-            className="p-2 rounded-md w-[416px] bg-slate-700 text-slate-100 border-2 border-transparent"
+            className="p-2 rounded-md w-[416px] bg-slate-800 text-slate-100 border-2 border-transparent"
             type="password"
             placeholder="Digite sua Senha"
           />
