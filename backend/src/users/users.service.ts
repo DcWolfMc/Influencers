d2670114fae4a6ed2d9cf6e01136c16f5d33d34f
Daniel <danielmcolares@gmail.com>
2024-09-03 13:40:45 -0300

    fix: stop returning password hash

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -13,36 +13,44 @@ export class UsersService {
     const hashedPassword = await hash(password, 10);
 
     return this.databaseService.$transaction(async (prisma) => {
-      const user = prisma.user.create({
+      const user = await prisma.user.create({
         data: { email, name, password: hashedPassword },
       });
-      return user;
+      return this.removePassword(user);
     });
   }
 
   async findAll() {
-    return await this.databaseService.user.findMany({});
+    const users = await this.databaseService.user.findMany({});
+    return users.map(user => this.removePassword(user));
   }
 
   async findOne(id: number, { includeRefreshToken = false }: FindUserDto) {
-    return this.databaseService.user.findFirst({
+    const user = await this.databaseService.user.findFirst({
       where: { id },
       include: {
         refreshToken: includeRefreshToken,
       },
     });
+    return this.removePassword(user);
   }
 
   async update(id: number, updatedUser: Prisma.UserUpdateInput) {
-    return await this.databaseService.$transaction(async (prisma) => {
-      return prisma.user.update({
+    return this.databaseService.$transaction(async (prisma) => {
+      const user = await prisma.user.update({
         where: { id },
         data: updatedUser,
       });
+      return this.removePassword(user);
     });
   }
 
   async delete(id: number) {
     return await this.databaseService.user.delete({ where: { id } });
+  }
+
+  private removePassword(user: any) {
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
   }
 }
